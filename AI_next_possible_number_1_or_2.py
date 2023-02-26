9569b5fd2ff34cba769dd260f506596ac2281261
sh3dyz <shedyzcuthlexy@gmail.com>
2023-02-27 01:46:56 +0300

    Added descriptions

--- AI_next_possible_number_1_or_2.py
+++ AI_next_possible_number_1_or_2.py
@@ -2,9 +2,13 @@ import numpy as np
 from sklearn .linear_model import LogisticRegression
 import random
 
+# behind the game there is red and black color, so we will use 1 = red and 2 = black
+# and our AI will use atleast 20 past result to learn and guess next number if
+# its 1 or 2, (red or black)
+
 # below is our history from game
 history = [] #minmum of one sample
-for i in range(10):
+for i in range(20):
     history.append(random.randint(1,2)) # we add some samples
     
 def PredictNumber(history):
@@ -27,23 +31,26 @@ def PredictNumber(history):
     # print("Possible Next Number: ", nx_num)
     return nx_num
 
-win = 0
-los = 0
-totg = int(input("Enter number of games: "))
-winp = 0
-losp = 0
+win = 0 # total win
+los = 0 # total loss
+totg = int(input("Enter number of games: ")) # total game
+winp = 0 # win %
+losp = 0 # loss %
 
 for i in range(totg):
     pn = PredictNumber(history)
     rn = int(random.randint(1, 2))
     history.append(rn)
+    del(history[0]) # we need to learn from few history
     
     # lets verify if AI win or Not
     if pn == rn:
         win += 1
     else:
         los += 1
-winp = (win / totg) * 100
+
+# calculate percantage
+winp = (win / totg) * 100 
 losp = (los / totg) * 100
 print("AI Win : {}\nAI Loss : {}\nTotal Games : {}".format(win, los, totg))
 print("Win Percentage: {}%\nLoss percantage: {}%".format(int(winp), int(losp)))
