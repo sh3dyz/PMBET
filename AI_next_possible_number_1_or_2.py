3feb0106e7a561998789269e6e22270bd959d51a
sh3dyz <shedyzcuthlexy@gmail.com>
2023-02-27 01:26:23 +0300

    some updates

--- AI_next_possible_number_1_or_2.py
+++ AI_next_possible_number_1_or_2.py
@@ -3,8 +3,10 @@ from sklearn .linear_model import LogisticRegression
 import random
 
 # below is our history from game
-history = [1, 2, 2, 1, 2, 1, 2, 1, 1, 2]
-
+history = [] #minmum of one sample
+for i in range(10):
+    history.append(random.randint(1,2)) # we add some samples
+    
 def PredictNumber(history):
     #convert that history to a numpy array
     X = np.array(history[:-1]).reshape(-1, 1)
@@ -24,14 +26,24 @@ def PredictNumber(history):
     # #lets print next number
     # print("Possible Next Number: ", nx_num)
     return nx_num
-    
-for i in range(10):
+
+win = 0
+los = 0
+totg = int(input("Enter number of games: "))
+winp = 0
+losp = 0
+
+for i in range(totg):
     pn = PredictNumber(history)
     rn = int(random.randint(1, 2))
     history.append(rn)
     
     # lets verify if AI win or Not
     if pn == rn:
-        print("AI Win!")
+        win += 1
     else:
-        print("AI Loss!")
+        los += 1
+winp = (win / totg) * 100
+losp = (los / totg) * 100
+print("AI Win : {}\nAI Loss : {}\nTotal Games : {}".format(win, los, totg))
+print("Win Percentage: {}%\nLoss percantage: {}%".format(int(winp), int(losp)))
