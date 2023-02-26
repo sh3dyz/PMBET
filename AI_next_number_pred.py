38b1377a9842246b9afdd11255719a1135c5c837
sh3dyz <shedyzcuthlexy@gmail.com>
2023-02-27 00:16:15 +0300

    Update AI code!

--- AI_next_number_pred.py
+++ AI_next_number_pred.py
@@ -2,12 +2,12 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # Define past numbers as input data
-xtrain = [[1], [2], [3], [4]]
-print(f"X Trained Data : {train}")
-X_train = np.array(train)
+xtrain = [[1], [3], [4], [6], [7]]
+print(f"X Trained Data : {xtrain}")
+X_train = np.array(xtrain)
 
 # Define next numbers as output data
-ytrain = [6, 7, 8, 9]
+ytrain = [9,10,12,13,15]
 print(f"Y Trained Data: {ytrain}")
 y_train = np.array(ytrain)
 
