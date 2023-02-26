6c9caa9fb86d0af82a44ce5d99275be90bfd0894
sh3dyz <shedyzcuthlexy@gmail.com>
2023-02-27 00:36:14 +0300

    Now AI can guess atleast half of total games

--- AI_next_possible_number_1_or_2.py
+++ AI_next_possible_number_1_or_2.py
@@ -1,22 +1,30 @@
 import numpy as np
 from sklearn .linear_model import LogisticRegression
+import random
+
 # below is our history from game
 history = [1, 2, 2, 1, 2, 1, 2, 1, 1, 2]
 
-#convert that history to a numpy array
-X = np.array(history[:-1]).reshape(-1, 1)
+def PredictNumber(history):
+    #convert that history to a numpy array
+    X = np.array(history[:-1]).reshape(-1, 1)
 
-#create array for the label - next number according to our history
-Y = np.array(history[1:])
+    #create array for the label - next number according to our history
+    Y = np.array(history[1:])
 
-#lets create logistic regression model and fit it to the data
-md = LogisticRegression()
-md.fit(X, Y)
+    #lets create logistic regression model and fit it to the data
+    md = LogisticRegression()
+    md.fit(X, Y)
 
-#lets use the above model to predict next number
-nx_num = md.predict(np.array([history[-1]]).reshape(-1, 1))[0]
+    #lets use the above model to predict next number
+    nx_num = md.predict(np.array([history[-1]]).reshape(-1, 1))[0]
 
-#show history
-print("HISTORY: ", history)
-#lets print next number
-print("Possible Next Number: ", nx_num)
+    #show history
+    print("HISTORY: ", history)
+    #lets print next number
+    print("Possible Next Number: ", nx_num)
+    
+for i in range(10):
+    rn = int(random.randint(1, 2))
+    history.append(rn)
+    PredictNumber(history)
